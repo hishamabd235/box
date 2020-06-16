1ef45c32cf85f4e9ddf50ca4eda7606263d66f1f
hishamabd235 <65910788+hishamabd235@users.noreply.github.com>
2020-06-16 18:56:21 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,17 @@
+class Ground{
+constructor(){
+    var options = {
+        isStatic:true      
+     }
+     this.body = Bodies.rectangle(200,380,400,30,options);
+     World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill(255);
+      rect(pos.x, pos.y,400,30);
+      pop();
+    }
+}
+
